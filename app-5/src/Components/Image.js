d74e2295265303189852a6aa9c7688d0c9fa3af3
Makenna Garrett <kenna.dunford@gmail.com>
2021-03-03 20:36:53 -0700

    completed app 5

---
+++ app-5/src/Components/Image.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Image extends Component {
+  render(){
+    return (
+      <div>
+        <img className='image' src='https://images.unsplash.com/photo-1503489319033-e9ef2959d8cc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Y2F0cyUyMGZpZ2h0aW5nfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'></img>
+        <h1>409</h1>
+        <h4>Conflict</h4>
+      </div>
+    )
+  }
+}
+
+export default Image;
